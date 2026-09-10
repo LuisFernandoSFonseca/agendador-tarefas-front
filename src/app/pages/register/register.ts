bf27385beb462c9e7c18874ce7c9c0084f25d105
LuisFernandoSFonseca <luisf_sanches@live.com>
2026-09-10 15:27:56 -0300

    Adding some forms validation

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -5,7 +5,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { PasswordField } from '../../shared/components/password-field/password-field';
-import { ReactiveFormsModule, FormBuilder, FormGroup, FormControl} from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   imports: [MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule, PasswordField, ReactiveFormsModule],
@@ -18,17 +18,21 @@ export class Register {
   form: FormGroup;
   constructor(private formBuilder: FormBuilder) {
     this.form = this.formBuilder.group({
-      fullName: [''],
-      email: [''],
-      password: ['']
+      fullName: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
     })
   }
-  
-  get passwordControl() : FormControl {
+
+  get passwordControl(): FormControl {
     return this.form.get('password') as FormControl;
   }
 
   submit() {
-    console.log(this.form.value)
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return
+    }
+    console.log("Formulário submetido", this.form.value)
   }
 }
