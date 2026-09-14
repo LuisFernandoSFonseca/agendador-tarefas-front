d05dcc12718f677401cbf193a6fa0402a91f69e5
LuisFernandoSFonseca <luisf_sanches@live.com>
2026-09-14 14:27:36 -0300

    Created app top menu

---
+++ src/app/shared/components/global/top-menu/top-menu.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink, RouterModule } from '@angular/router';
+
+@Component({
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterLink, RouterModule],
+  selector: 'app-top-menu',
+  styleUrl: './top-menu.scss',
+  templateUrl: './top-menu.html',
+})
+export class TopMenu {
+  appLogo = "assets/logo-agendador-javanauta.png";
+}
